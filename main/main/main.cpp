9dc04a5eb9bc5f16f3b0504fff298f3ace193295
даниил герасимов <dana-gerasimov.gerasimov@yandex.ru>
2024-04-17 10:22:08 +0300

    tretia function

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -10,20 +10,20 @@ void EnterY(double &num_Y)
 }
 void ArtimicMean(double num_X, double num_Y )
 {
-
+    cout << "Srednee znachenie:" << (num_X + num_Y) / 2 << endl;
 }
-int GeometricMean(double num_X, double num_Y)
+void GeometricMean(double num_X, double num_Y)
 {
 
 }
 int main()
 {
     int n = 0;
-    double x, y, arg, geom;
-    cout << "1.Enter a number X";
-    cout << "2.Enter a number Y";
-    cout << "3.Calculate arithmetic mean";
-    cout << "4.Calculate geometric mean";
+    double x, y;
+    cout << "1.Enter a number X"<< endl;
+    cout << "2.Enter a number Y" << endl;
+    cout << "3.Calculate arithmetic mean" << endl;
+    cout << "4.Calculate geometric mean" << endl;
     cout << "Please enter number of the desired function or 0 if you want close the app:";
     cin >> n;
     switch (n)
