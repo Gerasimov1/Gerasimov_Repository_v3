f2191583e995d872144a5da891aebf59943caa67
даниил герасимов <dana-gerasimov.gerasimov@yandex.ru>
2024-04-17 10:08:26 +0300

    Vtora function

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -6,30 +6,34 @@ void EnterX(double &num_X)
 }
 void EnterY(double &num_Y)
 {
-
+    do
+    {
+        cin >> num_Y;
+    } 
+    while (num_Y <= 0);
 }
 void ArtimicMean(double num_X, double num_Y )
 {
 
 }
-int GeometricMean(double num_X, double num_Y)
+void GeometricMean(double num_X, double num_Y)
 {
 
 }
 int main()
 {
     int n = 0;
-    double x, y, arg, geom;
-    cout << "1.Enter a number X";
-    cout << "2.Enter a number Y";
-    cout << "3.Calculate arithmetic mean";
-    cout << "4.Calculate geometric mean";
+    double x, y;
+    cout << "1.Enter a number X"<<endl;
+    cout << "2.Enter a number Y" << endl;
+    cout << "3.Calculate arithmetic mean" << endl;
+    cout << "4.Calculate geometric mean" << endl;
     cout << "Please enter number of the desired function or 0 if you want close the app:";
     cin >> n;
     switch (n)
     {
         case 1:
-            EnterY(y);
+            EnterX(x);
         case 2:
             EnterY(y);
         case 3:
